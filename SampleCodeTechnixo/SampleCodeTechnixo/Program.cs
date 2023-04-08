48b7e30fe28f31344aec377e71950471bcaeb026
Khanh Sy <sykhanh1996@gmail.com>
2023-04-09 00:35:17 +0700

    sample code hackerrank

---
+++ SampleCodeTechnixo/SampleCodeTechnixo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+fizzBuzz(15);
+static void fizzBuzz(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+            continue;
+        }
+        if (i % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+            continue;
+        }
+        if (i % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+            continue;
+        }
+   
+
+        Console.WriteLine(i);
+
+    }
+}
+Console.ReadLine();
