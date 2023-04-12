2d7c255ab43357384e25d9a1753ec146ca7cc007
Khanh Sy <sykhanh1996@gmail.com>
2023-04-12 22:27:05 +0700

    1672

---
+++ SampleCodeTechnixo/1672.RichestCustomerWealth/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[][] a = {
+    new [] { 1, 5 },
+    new [] { 7, 3 },
+    new [] { 3, 5 }
+};
+var test = MaximumWealth(a);
+int MaximumWealth(int[][] accounts)
+{
+    var dic = new Dictionary<int, int>();
+    for (int i = 0; i < accounts.Length; i++)
+    {
+        var wealth = 0;
+        for (int j = 0; j < accounts[i].Length; j++)
+        {
+            wealth += accounts[i][j];
+        }
+        dic[i] = wealth;
+    }
+    var max = dic.Values.Max(); ;
+
+    return max;
+}
