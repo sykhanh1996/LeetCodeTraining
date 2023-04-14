cafe498b6bf9b4c2250c262e45ef886947032647
Khanh Sy <sykhanh1996@gmail.com>
2023-04-14 23:32:39 +0700

    387. update

--- SampleCodeTechnixo/387.FirstUniqueCharacterinaString/Program.cs
+++ SampleCodeTechnixo/387.FirstUniqueCharacterinaString/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var result = firstUniqChar("loveleetcode");
+Console.WriteLine(result);
+Console.ReadLine();
+int firstUniqChar(string s)
+{
+    var dic = new Dictionary<int, string>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        dic[i] = s[i].ToString();
+    }
+
+    var distinct = dic.Distinct();
+    foreach (var item in distinct.OrderBy(x=>x.Key))
+    {
+        return item.Key;
+    }
+
+    return -1;
+}
